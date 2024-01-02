af2ae75e21af17c90f2d6063e4390821e36ebd61
Jonathan Clem <j@jclem.me>
2024-01-02 13:09:37 -0500

    Add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
+import sitemap from '@astrojs/sitemap';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/static";
 import { defineConfig } from "astro/config";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   output: "static",
   adapter: vercel(),
   site: process.env.NODE_ENV === 'production'
